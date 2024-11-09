c56057a4c0fab34fd5f46ea6023391de0954ee99
Dionisis Karakasiliotis <dion.k.karak@gmail.com>
2024-11-09 20:37:03 +0200

    Update Logo.tsx

---
+++ Logo.tsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+interface LogoProps {
+  url: string;
+}
+
+export default function Logo({ url }: LogoProps) {
+  return (
+    <StyledLogo>
+      <img src={url} alt="" />
+      <a href="/">Home</a>
+    </StyledLogo>
+  );
+}
+
+const StyledLogo = styled.figure`
+  display: flex;
+  width: 80px;
+  position: relative;
+  margin: 0;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+
+  a {
+    position: absolute;
+    inset: 0;
+    opacity: 0;
+  }
+`;
